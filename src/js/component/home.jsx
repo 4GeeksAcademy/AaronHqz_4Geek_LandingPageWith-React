3e1ca6862ada47103e182a1b5eaae4099e8358a9
Aaron Henriquez <60531374+aaronhqz@users.noreply.github.com>
2023-07-22 01:16:56 +0000

    saving

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  let [listItems, setListItems] = useState([]);
+
+  let [navBarItems, netNavBarItems] = useState([
+    { text: "Home", href: "/" },
+    { text: "Products", href: "/products" },
+    { text: "FAQs", href: "/faqs" },
+    {
+      text: "Contact",
+      items: [
+        { text: "Sales", href: "/sales" },
+        { text: "Support", href: "support" },
+        { text: "Non Profit", href: "nonprofit" },
+      ],
+    },
+    { text: "About", href: "about" },
+  ]);
+
+  return (
+    <div className="text-center">
+		<NavBar items ></NavBar>
+      <h1 className="text-center mt-5">Hello Rigo!</h1>
+      <p>
+        <img src={rigoImage} />
+      </p>
+      <a href="#" className="btn btn-success">
+        If you see this green button... bootstrap is working...
+      </a>
+      <p>
+        Made by <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+        love!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
